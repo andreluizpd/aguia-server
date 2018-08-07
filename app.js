00ab134c067541871de0fac04abd97f2b46d0874
André Luiz Pastore Dambros <andreluizpdsl@gmail.com>
2018-08-07 16:12:41 -0300

    alterada a origem das requisições

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const birl    = require('./code_exec.js');
 const verif   = require('./code_verification.js');
 
 var corsOptions = {
-    origin: 'null'
+    origin: 'https://andreluizpd.github.io'
 };
 
 // Permitindo CORS
